380aea3e84f992e96d6caad91fef05be83f9cdd3
Couson <couson@foxmail.com>
2019-02-26 00:39:42 -0800

    word saved

---
+++ Flix/MovieGridwCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridwCell.swift
+//  Flix
+//
+//  Created by Yifei Ning on 2/25/19.
+//  Copyright © 2019 Yifei Ning. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridwCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
